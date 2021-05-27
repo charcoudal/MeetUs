168668d4e3f0e04e71e616a242aa2183622cc6e4
charcoudal <charlottejones2022@u.northwestern.edu>
2021-05-27 00:05:46 -0500

    expotemplate

---
+++ js/index.js
@@ -0,0 +1,59 @@
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    var x2 = document.getElementById("myDIV2");
+    var x3 = document.getElementById("myDIV3");
+    var x4 = document.getElementById("myDIV4");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      x2.style.display = "none";
+      x3.style.display = "none";
+      x4.style.display = "none";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction2() {
+    var x = document.getElementById("myDIV2");
+    var x1 = document.getElementById("myDIV");
+    var x3 = document.getElementById("myDIV3");
+    var x4 = document.getElementById("myDIV4");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      x1.style.display = "none";
+      x3.style.display = "none";
+      x4.style.display = "none";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction3() {
+    var x = document.getElementById("myDIV3");
+    var x1 = document.getElementById("myDIV");
+    var x2 = document.getElementById("myDIV2");
+    var x4 = document.getElementById("myDIV4");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      x1.style.display = "none";
+      x2.style.display = "none";
+      x4.style.display = "none";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction4() {
+    var x = document.getElementById("myDIV4");
+    var x1 = document.getElementById("myDIV");
+    var x2 = document.getElementById("myDIV2");
+    var x3 = document.getElementById("myDIV3");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      x1.style.display = "none";
+      x2.style.display = "none";
+      x3.style.display = "none";
+    } else {
+      x.style.display = "none";
+    }
+  }
