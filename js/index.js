0aa2905a5276391c3eed39c7af98ed4f3963bb36
cristinabarclay <57467401+cristinabarclay@users.noreply.github.com>
2021-05-28 16:38:10 -0500

    exp

--- js/index.js
+++ js/index.js
@@ -1,59 +1,16 @@
-function myFunction() {
-    var x = document.getElementById("myDIV");
-    var x2 = document.getElementById("myDIV2");
-    var x3 = document.getElementById("myDIV3");
-    var x4 = document.getElementById("myDIV4");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-      x2.style.display = "none";
-      x3.style.display = "none";
-      x4.style.display = "none";
-    } else {
-      x.style.display = "none";
+function openJob(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
     }
-  }
-
-  function myFunction2() {
-    var x = document.getElementById("myDIV2");
-    var x1 = document.getElementById("myDIV");
-    var x3 = document.getElementById("myDIV3");
-    var x4 = document.getElementById("myDIV4");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-      x1.style.display = "none";
-      x3.style.display = "none";
-      x4.style.display = "none";
-    } else {
-      x.style.display = "none";
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
   }
-
-  function myFunction3() {
-    var x = document.getElementById("myDIV3");
-    var x1 = document.getElementById("myDIV");
-    var x2 = document.getElementById("myDIV2");
-    var x4 = document.getElementById("myDIV4");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-      x1.style.display = "none";
-      x2.style.display = "none";
-      x4.style.display = "none";
-    } else {
-      x.style.display = "none";
-    }
-  }
-
-  function myFunction4() {
-    var x = document.getElementById("myDIV4");
-    var x1 = document.getElementById("myDIV");
-    var x2 = document.getElementById("myDIV2");
-    var x3 = document.getElementById("myDIV3");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-      x1.style.display = "none";
-      x2.style.display = "none";
-      x3.style.display = "none";
-    } else {
-      x.style.display = "none";
-    }
-  }
+  
+  // Get the element with id="defaultOpen" and click on it
+  document.getElementById("defaultOpen").click();
